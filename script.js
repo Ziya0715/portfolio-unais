db9b44005585aeafa12cc764457810a1b823b994
unais <unaisziyauddeen@gmail.com>
2025-07-05 10:33:25 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Fade-in animation on scroll
+const fadeEls = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+fadeEls.forEach(el => observer.observe(el));
